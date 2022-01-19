4684807551f9843a9e057800bbdbc08b7fab81e4
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2022-01-19 03:20:39 -0600

    validators

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,50 +1,56 @@
-const { Schema, model } = require('mongoose');
- // create the Pizza model using the PizzaSchema
- const dateFormat = require('../utils/dateFormat');
- 
+const { Schema, model } = require("mongoose");
+// create the Pizza model using the PizzaSchema
+const dateFormat = require("../utils/dateFormat");
 
- const PizzaSchema = new Schema(
-    {
-      pizzaName: {
-        type: String
-      },
-      createdBy: {
-        type: String
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => dateFormat(createdAtVal)
-      },
-      size: {
-        type: String,
-        default: 'Large'
-      },
-      toppings: [],
-      comments: [
-        {
-          type: Schema.Types.ObjectId,
-          ref: 'Comment'
-        }
-      ]
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-      toJSON: {
-        virtuals: true,
-        getters: true
+    createdBy: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      required: true,
+  enum: ['Personal', 'Small', 'Medium', 'Large', 'Extra Large'],
+  default: 'Large'
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
       },
-      id: false
-    }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.reduce(
+    (total, comment) => total + comment.replies.length + 1,
+    0
   );
+});
 
-  // get total count of comments and replies on retrieval
-  PizzaSchema.virtual('commentCount').get(function() {
-    return this.comments.reduce((total, comment) => total + comment.replies.length + 1, 0);
-  });
-
-  
-
-  const Pizza = model('Pizza', PizzaSchema);
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;
